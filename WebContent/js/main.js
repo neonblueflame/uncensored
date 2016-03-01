647b41e7ddecd15d2001dbd7e1f9c0fbc321e361
Blueflamey <neonblueflame@gmail.com>
2016-03-02 04:50:05 +0800

    Fixed changing title

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-	var mainTitle = $("#title-container>h1").html();
-	var mainSubTitle = $("#title-container>h3").html();
+	var mainTitle = $(".title-container>h1").html();
+	var mainSubTitle = $(".title-container>h3").html();
 	
 	var setToUnscensored = function() {		
-		$("#title-container>h1").html(mainTitle);
-		$("#title-container>h3").html(mainSubTitle);
+		$(".title-container>h1").html(mainTitle);
+		$(".title-container>h3").html(mainSubTitle);
 
 		/* -- Scroll code
 		var articleID = this.id.replace("link-", "");
@@ -18,8 +18,8 @@ $(document).ready(function() {
 	$("#link-artworks").click(setToUnscensored);
 	
 	$("#link-artist").click(function() {
-		$("#title-container>h1").html("Rayne Mariano");
-		$("#title-container>h3").html("the artist - " +
+		$(".title-container>h1").html("Rayne Mariano");
+		$(".title-container>h3").html("the artist - " +
 			"<span class='italicized'>kuno</span>");
 	});
 });
