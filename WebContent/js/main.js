df47bfe16d2d01cce390b80a980b4bc874dec3e9
Blueflamey <neonblueflame@gmail.com>
2016-03-01 10:06:00 +0800

    Fixed border problem, added title, removed main.js code

--- WebContent/js/main.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-	setTimeout(updateScrollSpy, 3000);
-});
-function updateScrollSpy() {
-	jQuery('[data-spy="scroll"]').each(function() {
-		var $spy = jQuery(this).scrollspy('refresh')
-	});
-}
